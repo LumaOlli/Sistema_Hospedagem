385347998c7374b391be949679bb8cd612f16fe6
Luma Oliveira <111361940+LumaOl@users.noreply.github.com>
2022-10-22 17:44:10 -0300

    add file

---
+++ hospedagem.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Bedroom bedroom;
+typedef struct Guest guest;
+
+guest *  creat_reservation(char *reverse_data);
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "hospedagem.h"
+
+int main(void){
+    guest *g=  creat_reservation("reverse_data");
+
+    return 0;
+}
