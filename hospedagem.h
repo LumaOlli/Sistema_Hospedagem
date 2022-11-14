52df9cc408b8e1d9ed9debcf61b922ed43cd35a9
LumaOlli <lumaoliveira337@gmail.com>
2022-11-14 20:09:51 -0300

    /**/

--- hospedagem.h
+++ hospedagem.h
@@ -6,14 +6,14 @@ typedef struct hospede Hospede;
 
 typedef struct lista Lista;
 
-//Função que recebe como parâmetro nova reserva do tipo hospede, onde vai ser armazenado os dados dos hospedes
+/*Função que recebe como parâmetro nova reserva do tipo hospede, onde vai ser armazenado os dados dos hospedes*/
 Hospede *realizar_reserva(Hospede *nova_reserva);
 
-//Função que recebe como parametro nova reserva e elemento, onde vai ser percorrida a lista para que a reserva
-//de um determinado hospede seja excluida
+/*Função que recebe como parametro nova reserva e elemento, onde vai ser percorrida a lista para que a reserva
+de um determinado hospede seja excluida*/
 Lista *excluir_reserva(Lista *nova_reserva, int elemento);
 
-//
+/**/
 void listar_reserva();
 
 Lista *buscar_reserva(int reserva, Lista *nova_reserva);
