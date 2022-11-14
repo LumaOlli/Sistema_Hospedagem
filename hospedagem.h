a8c17161e957f56d78ab76f506d68eae44eaa4e6
AmandintaC <amandacristinagomes8@gamil.com>
2022-11-14 15:31:34 -0300

    comentario da função realizar reserva

--- hospedagem.h
+++ hospedagem.h
@@ -6,7 +6,7 @@ typedef struct hospede Hospede;
 
 typedef struct lista Lista;
 
-//Função que , onde a reserva de cada hosede vai ser armazenado em uma lista encadeada para armazernar seus dados
+//Função que recebe como parâmetro nova reserva do tipo hospede, onde vai ser armazenado os dados dos hospedes
 Hospede *realizar_reserva(Hospede *nova_reserva);
 //Função que recebe como parametro nova reserva e elemento
 Lista *excluir_reserva(Lista *nova_reserva, int elemento);
