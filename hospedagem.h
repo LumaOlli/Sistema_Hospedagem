629ab43158d314ebc126a0528531311752c311c1
AmandintaC <amandacristinagomes8@gamil.com>
2022-11-14 15:43:22 -0300

    comentario na funçao de excluir

--- hospedagem.h
+++ hospedagem.h
@@ -8,9 +8,12 @@ typedef struct lista Lista;
 
 //Função que recebe como parâmetro nova reserva do tipo hospede, onde vai ser armazenado os dados dos hospedes
 Hospede *realizar_reserva(Hospede *nova_reserva);
-//Função que recebe como parametro nova reserva e elemento
+
+//Função que recebe como parametro nova reserva e elemento, onde vai ser percorrida a lista para que a reserva
+//de um determinado hospede seja excluida
 Lista *excluir_reserva(Lista *nova_reserva, int elemento);
 
+//
 void listar_reserva();
 
 Lista *buscar_reserva(int reserva, Lista *nova_reserva);
