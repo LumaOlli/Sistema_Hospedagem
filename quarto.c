aece3494781fcdcd8def5c3a32b21db1c21652eb
AmandintaC <amandacristinagomes8@gamil.com>
2022-11-19 16:18:47 -0300

    122, aí eu vou gritar oq? feijão com arroz

--- quarto.c
+++ quarto.c
@@ -3,35 +3,3 @@
 #include <string.h>
 #include "quarto.h"
 
-<<<<<<< HEAD
-struct quarto{
-  
-}
-
-void ler_arq(){
-  FILE *arq;
-  char Linha[100];
-  char *resultado;
-  int i;
-
-  arq=fopen("Quarto.txt", "rt"); /*Abre o arquivo TEXTO para leitura*/
-  if(arq == NULL){  /*Caso tenha erro na abertura do arquivo*/
-    printf("Erro na abertura do arquivo");
-    return;
-  }
-
-  i=1;
-  while(!feof(arq)){/*Lê linha(inclusive com '/n')*/
-    /*<FEOF> retorna um valor dif. de zero se uma operação de
-    leitura tentou ler após o final do arquivo. Caso contrário
-    return 0.*/
-    resultado=fgets(Linha, 100, arq);/*O fgets lê ate 99  caracteres e/ou '/n'*/
-    if(resultado){/*Caso a leitura for bem sucedida*/
-      printf("%s", i, Linha);
-      i++;
-    }
-  }
-  fclose(arq);
-}
-=======
->>>>>>> 0a7344c04ac4f99435a68246ca5cc5d89f6ebc23
