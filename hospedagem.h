4f7baecd6ff283b3b06a3e7b2542d860ff9cf4cf
AmandintaC <amandacristinagomes8@gamil.com>
2022-11-22 15:04:40 -0300

    o outro lado do código

--- hospedagem.h
+++ hospedagem.h
@@ -6,33 +6,36 @@ typedef struct hospede Hospede;
 
 typedef struct lista Lista;
 
-/*Função que recebe como parâmetro nova reserva do tipo hospede, onde vai ser armazenado os dados dos hospedes*/
+/*Função que recebe como parâmetro um vazio, onde vai ser armazenado os dados dos hospedes*/
 Hospede *realizar_reservaH(void);
 
-/*Função que recebe como parametro nova reserva e elemento, onde vai ser percorrida a lista para que a reserva
-de um determinado hospede seja excluida*/
-Lista *excluir_reservaH(Lista *nova_reserva, int elemento);
+/*Função que recebe como parametro as variáveis lista do tipo Lista e numero informado do tipo int, 
+onde vai ser percorrida a lista para que a reserva de um determinado hospede seja excluida, retorna a lista atualizada*/
+Lista* excluir_reservaH(Lista *lista, int numero_informado);
 
 /*Função listar para imprimir os dados do hospede*/
 void listar_reservaH();
 
-/**/
+/*Função que recebe como parâmetro lista do tipo Lista, e elemento do tipo Hospede, onde vai ser inserido e ordenado em uma lista
+os dados do hospede, retorna a lista*/
 Lista * insere_hospede(Lista *lista, Hospede *elemento);
 
-/**/
+/*Função vazia que retona NULL*/
 Lista* cria_ls_hosp(); 
 
-/**/
-Lista* excluir_reservaH(Lista *lista, int elemento);
-
-/**/
+/*Função que recebe como parâmetro a variável lista do tipo lista, onde vai ser imprimido na tela os dados do hospede*/
 void Listar_reservaH(Lista* lista);
 
-/*Função que irá buscar uma reserva específica na lista dos hospedes do hotel e retornar a lista atualizada*/
+/*Função que recebe como parâmetros numero informado do tipo int e lista do tipo Lista, a função irá buscar uma 
+reserva específica na lista dos hospedes do hotel e retornar a lista atualizada*/
 void Buscar_reservaH(int numero_informado, Lista *lista);
 
-/*Função encarregada de editar/atualizar um reserva do hotel*/
+/*Função que recebe como parâmetros lista do tipo Lista e numero informado do tipo int, 
+é encarregada de editar/atualizar um reserva do hotel*/
 void Editar_reservaH(Lista *lista, int numero_informado);
 
-/*Função que irá verificar se a lista esta vazia e irá retornar a mesma*/
-int VaziaH(Lista *nova_reserva);
+/*Função que recebe como parâmetro lista do tipo lista, e irá verificar se a lista esta vazia e irá retornar a mesma*/
+int lista_vazia(Lista *lista);
+
+/*Função que recebe como parâmetro lista do tipo lista, responsável por verificar quantos hospedes tem na lista*/
+void quantitativo_de_hospede(Lista *lista);
